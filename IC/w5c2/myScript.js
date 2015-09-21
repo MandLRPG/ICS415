0e51f478ab1866f3ed4fd5226e69adee12881a41
MandLRPG <knaka@hawaii.edu>
2015-09-21 10:02:23 -1000

    Started classwork for 9/23

---
+++ IC/w5c2/myScript.js
@@ -0,0 +1,49 @@
+/**
+ * Created by uninstall on 9/21/15.
+ */
+
+$(document).ready(function() {
+  $("#tab1Text").mouseenter(function() {
+    $(this).text("Eeek!");
+  });
+
+  $("#tab1Text").mouseleave(function() {
+    $(this).text("This is where the date goes!");
+  });
+
+  $("#formsButton").click(function() {
+    window.location.href = "forms.html";
+  });
+
+  $("#dateButton").click(function() {
+    $("#tab1Text").text(displayDate());
+  });
+
+  $("#hideText").click(function () {
+    $(this).hide();
+    $("#showButton").show();
+  });
+
+  $("#showButton").click(function () {
+    $(this).hide();
+    $("#hideText").show();
+  });
+});
+
+function displayDate() {
+  return new Date();
+}
+
+function validate() {
+  var x, text;
+
+  x = document.getElementById("helloBox").value;
+
+  if(x == "Hello!") {
+    text = "Hello to you too!";
+  }
+  else {
+    text = "I asked for a Hello! :(";
+  }
+  document.getElementById("answer").innerHTML = text;
+}
